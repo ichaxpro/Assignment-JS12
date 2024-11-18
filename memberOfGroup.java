2e0844fd1b3c75a0a4bf06a2dcb4f785d96f9f83
fidelac <clarisafidela@gmail.com>
2024-11-18 17:17:15 +0700

    code nama group

---
+++ memberOfGroup.java
@@ -0,0 +1,8 @@
+public class memberOfGroup {
+    public static void main(String[] args) {
+        System.out.println("Our group is: ");
+        System.out.println("1. Dewi Chalissa Rania (08)");
+        System.out.println("2. Fidela Clarisa (13)");
+        System.out.println("3. Nahda Auliya Raudhatunnisa (21)");
+    }
+}
